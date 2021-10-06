e0de588ba73508bf865de61d46e60e583b0663a2
Luis <lfmnovaes@gmail.com>
2021-10-05 23:48:05 -0300

    Add dependencies and some cleaning
    * Add all necessary dependencies
    * Add linter files
    * Update README
    * Initial html commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-import _ from 'lodash';
-import printMe from './print.js';
 import './style.css';
 
 function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+  console.log("hello world");
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
