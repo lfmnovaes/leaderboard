4dbfdc082bf3b3fbda904084464ff8d3163bdcb4
Luis <lfmnovaes@gmail.com>
2021-10-06 02:33:01 -0300

    Template
    * Build template from injecting javascript with webpack
    * Add basic template with form and placeholder scores
    * Use bootstrap for style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,63 @@
 import './style.css';
 
-function component() {
-  console.log("hello world");
-}
+const main = document.getElementsByTagName('main');
 
-document.body.appendChild(component());
+window.refreshAll = () => {
+  console.log('refresh');
+};
+
+window.addScore = () => {
+  console.log('add score');
+};
+
+const content = `
+<section class="container-fluid">
+  <div class="d-flex justify-content-between">
+    <h2>Recent scores</h2>
+    <button type="button" class="btn btn-outline-primary" onclick="refreshAll()">Refresh</button>
+  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Score</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>100</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>85</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>50</td>
+      </tr>
+    </tbody>
+  </table>
+</section>
+<aside class="container-fluid">
+  <h2>Add  your score</h2>
+  <form>
+    <div class="mb-3">
+      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Your Name">
+    </div>
+    <div class="mb-3">
+      <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Your Score">
+    </div>
+    <button type="button" class="btn btn-primary" onclick="addScore()">Submit</button>
+  </form>
+</aside>
+`;
+
+main[0].insertAdjacentHTML('beforeend', content);
+
+const scoreboard = document.getElementsByTagName('tbody');
+console.log(scoreboard[0]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
+module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
